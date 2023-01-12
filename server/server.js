fd1fa049b8f2aca73fcf01efa12c3dec77e0c1c4
Kerra Springgay <kerraspring@gmail.com>
2023-01-11 21:17:01 -0600

    server.js pseudo-code

--- server/server.js
+++ server/server.js
@@ -4,6 +4,18 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// use .env file
+
+// require("./config/passport")(passport);
+
+// connectDB()
+
+// app.use session, passport
+
+// use public folder
+
+// GETs
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
