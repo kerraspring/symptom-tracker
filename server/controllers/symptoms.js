30cf5658e39318c599b1262aa8dbd6941b306aab
Stefan <stefantaitano@gmail.com>
2023-01-21 13:09:31 -0700

    add method to getSymptom in symptoms controller

--- server/controllers/symptoms.js
+++ server/controllers/symptoms.js
@@ -1,1 +1,12 @@
 const Symptom = require('../models/Symptom');
+
+module.exports = {
+    getSymptom: async (req,res) => {
+        try {
+            const symptom = await Symptom.findById(req.params.id);
+            //render with react
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
